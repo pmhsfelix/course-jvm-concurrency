0078ab6f42264f05d3032f7cf61b28b78fba2f0a
Pedro Felix <pmhsfelix@gmail.com>
2025-03-04 14:55:18 +0000

    More incorrect synchronization tests

--- code/jvm/src/test/kotlin/org/pedrofelix/concurrency/course/basics/IncorrectSynchronizationTests.kt
+++ code/jvm/src/test/kotlin/org/pedrofelix/concurrency/course/basics/IncorrectSynchronizationTests.kt
@@ -1,6 +1,9 @@
 package org.pedrofelix.concurrency.course.basics
 
+import org.junit.jupiter.api.Assertions.assertTrue
+import java.util.Collections
 import java.util.LinkedList
+import java.util.NoSuchElementException
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
@@ -12,20 +15,26 @@ import kotlin.test.assertNotNull
  * Illustrates some less obvious errors when creating thread-safe data structures using locking.
  */
 class IncorrectSynchronizationTests {
+    interface SimpleListInterface<T> : Iterable<T> {
+        fun addTail(t: T)
+
+        fun removeHead(): T?
+    }
+
     /**
      * An incorrectly synchronized list implementation.
      * Can you identify the problem?
      */
-    class AListWithIncorrectSynchronization<T> : Iterable<T> {
+    class AListWithIncorrectSynchronization<T> : SimpleListInterface<T> {
         private val list = LinkedList<T>()
         private val lock = ReentrantLock()
 
-        fun addTail(t: T) =
+        override fun addTail(t: T) =
             lock.withLock {
                 list.addLast(t)
             }
 
-        fun getHead(): T? =
+        override fun removeHead(): T? =
             lock.withLock {
                 if (list.isEmpty()) {
                     null
@@ -40,8 +49,22 @@ class IncorrectSynchronizationTests {
             }
     }
 
+    /**
+     * A simple wrapper on Collections.synchronizedList.
+     * Also incorrect.
+     */
+    class WrapperOverASynchronizedList<T> : SimpleListInterface<T> {
+        private val list = Collections.synchronizedList(LinkedList<T>())
+
+        override fun addTail(t: T) = list.addLast(t)
+
+        override fun removeHead(): T? = if (list.isNotEmpty()) list.removeFirst() else null
+
+        override fun iterator(): Iterator<T> = list.iterator()
+    }
+
     @Test
-    fun `stress test`() {
+    fun `stress test on AListWithIncorrectSynchronization`() {
         // given: a list incorrectly implemented
         val theList = AListWithIncorrectSynchronization<Int>()
         // and: a way to record exceptions
@@ -59,7 +82,7 @@ class IncorrectSynchronizationTests {
         val removingThreads = List(N_OF_THREADS) {
             threadBuilder.start {
                 repeat(N_OF_REPS) {
-                    theList.getHead()
+                    theList.removeHead()
                 }
             }
         }
@@ -80,6 +103,7 @@ class IncorrectSynchronizationTests {
         // then: the iterating threads have exceptions thrown
         val exceptions = exceptionRecorder.getExceptions()
         assertNotEquals(0, exceptions.size)
+
         // and: those exceptions include ConcurrentModificationException
         val concurrentModificationException = exceptions.firstOrNull { it is ConcurrentModificationException }
         assertNotNull(concurrentModificationException)
@@ -91,6 +115,107 @@ class IncorrectSynchronizationTests {
         // just for learning purposes
         println(concurrentModificationException)
         println(nullPointerException)
+    }
+
+    @Test
+    fun `stress test on WrapperOverASynchronizedList`() {
+        // given: a list incorrectly implemented
+        val theList = WrapperOverASynchronizedList<Int>()
+        // and: a way to record exceptions
+        val exceptionRecorder = ExceptionRecorder()
+
+        // when: having threads inserting, removing, and iterating on the list
+        val threadBuilder = Thread.ofPlatform()
+        val nOfReps = 500 * N_OF_REPS
+        val insertingThreads = List(N_OF_THREADS) {
+            threadBuilder.start {
+                repeat(nOfReps) {
+                    theList.addTail(it)
+                }
+            }
+        }
+        val removingThreads = List(N_OF_THREADS) {
+            threadBuilder.start {
+                repeat(nOfReps) {
+                    theList.removeHead()
+                }
+            }
+        }
+        val iteratingThreads = List(N_OF_THREADS) {
+            threadBuilder.start {
+                repeat(nOfReps) {
+                    exceptionRecorder.run {
+                        // There is an error in the following line
+                        theList.sum()
+                    }
+                }
+            }
+        }
+
+        // and: waiting for all threads to complete
+        val threads = insertingThreads + removingThreads + iteratingThreads
+        threads.forEach { it.join() }
+
+        // then: the iterating threads have exceptions thrown
+        val exceptions = exceptionRecorder.getExceptions()
+        assertNotEquals(0, exceptions.size)
+
+        // and: those exceptions include ConcurrentModificationException
+        val concurrentModificationException = exceptions.firstOrNull { it is ConcurrentModificationException }
+        assertNotNull(concurrentModificationException)
+
+        // and: those exceptions include NullPointerException
+        val nullPointerException = exceptions.firstOrNull { it is NullPointerException }
+        assertNotNull(nullPointerException)
+
+        // just for learning purposes
+        println(concurrentModificationException)
+        println(nullPointerException)
+    }
+
+    @Test
+    fun `stress test on WrapperOverASynchronizedList removeHead`() {
+        // given: a list incorrectly implemented
+        val theList = WrapperOverASynchronizedList<Int>()
+        // and: a way to record exceptions
+        val exceptionRecorder = ExceptionRecorder()
+
+        // when: having threads inserting, removing, and iterating on the list
+        val threadBuilder = Thread.ofPlatform()
+        val nOfReps = 500 * N_OF_REPS
+        val insertingThreads = List(1) {
+            threadBuilder.start {
+                repeat(nOfReps) {
+                    theList.addTail(it)
+                }
+            }
+        }
+        val removingThreads = List(N_OF_THREADS) {
+            threadBuilder.start {
+                exceptionRecorder.run {
+                    repeat(nOfReps) {
+                        theList.removeHead()
+                    }
+                }
+            }
+        }
+
+        // and: waiting for all threads to complete
+        val threads = insertingThreads + removingThreads
+        threads.forEach { it.join() }
+
+        // then: the iterating threads have exceptions thrown
+        val exceptions = exceptionRecorder.getExceptions()
+        assertNotEquals(0, exceptions.size)
+
+        // and: those exceptions include IndexOutOfBoundsException or NoSuchElementException
+        val indexOutOfBoundsException = exceptions.firstOrNull { it is IndexOutOfBoundsException }
+        val noSuchElementException = exceptions.firstOrNull { it is NoSuchElementException }
+        assertTrue(indexOutOfBoundsException != null || noSuchElementException != null)
+
+        // just for learning purposes
+        println(indexOutOfBoundsException)
+        println(noSuchElementException)
     }
 
     class ExceptionRecorder {
@@ -111,7 +236,7 @@ class IncorrectSynchronizationTests {
     }
 
     companion object {
-        private const val N_OF_THREADS = 10
-        private const val N_OF_REPS = 10_000
+        private const val N_OF_THREADS = 5
+        private const val N_OF_REPS = 50_000
     }
 }
